7655b7afc7e32d27ef12babd488ef21f2244c53c
wuyenlin <finne0831@gmail.com>
2021-11-08 15:58:03 +0100

    added a few more useful tools and load config

--- set.sh
+++ set.sh
@@ -1,7 +1,10 @@
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install git tmux tmate vim xclip ranger neofetch python3
+sudo apt install git tmux tmate vim xclip ranger neofetch python3 ranger xorg dmenu tar unrar
+snap install spotify nvim lsd vlc
 
-sudo cp icons/* /usr/share/icons/
-sudo cp themes/* /usr/share/themes/
+sudo cp -r icons/* /usr/share/icons/
+sudo cp -r themes/* /usr/share/themes/
+
+dconf load / << config.conf

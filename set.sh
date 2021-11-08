4543ad99a919a15c5be2feebfde7ba5ad90a3b10
wuyenlin <finne0831@gmail.com>
2021-11-08 17:28:31 +0100

    reformattedg

--- set.sh
+++ set.sh
@@ -1,30 +1,39 @@
-sudo apt update
-sudo apt upgrade -y
+#!/usr/bin/bash
+sudo apt update && sudo apt upgrade -y
 
 # install important apps
-sudo apt install git tmux tmate vim xclip ranger neofetch python3 ranger xorg dmenu tar unrar apt-transport-https curl
+sudo apt install git ssh tmux tmate vim xclip ranger neofetch python3 ranger xorg dmenu tar unrar apt-transport-https curl openssh-server openssh-client python3-venv python3-pip npm
 snap install spotify nvim lsd vlc
+
 
 # add custom icons and themes
 echo "copying icon and theme files"
 sudo cp -r icons/* /usr/share/icons/
 sudo cp -r themes/* /usr/share/themes/
 
+
 # load configuration
 echo "loading configuration and setting up rc files"
-dconf load / << config.conf
 cp .bashrc ~/
 cp .vimrc ~/
 
+# setup theme
+dconf load / < config.conf
 
-# download vscode and brave pkg
 
+# download and install brave
+echo "Installing Brave browser"
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser
 
 
+# download and install vscode
+echo "Installing VS Code"
 wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 -O code.deb
-sudo dpkg -i code.deb
+sudo dpkg -i code.deb && rm code.deb
+
+
+echo ""
+echo "Done setup."
